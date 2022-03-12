3846e001dbef54fc677632fb8675edc8c2342dbf
Mohammed Feras Al Bahar <100701492+KamakiMoji@users.noreply.github.com>
2022-03-12 11:18:57 +0300

    done

--- CW1.playground/Contents.swift
+++ CW1.playground/Contents.swift
@@ -2,4 +2,12 @@ import UIKit
 
 // التمرين الأول
 // ClassWork 1
-
+let name = "Mohammed Al Bahar"
+var age = 16
+let birthyear = 2005
+2022 - birthyear
+print (" Hello my name is \(name) and my age is \(age)")
+var num1 = "8"
+var num2 = 4
+var num3 = 2.5
+num3 = (Double(num1) ?? 0) + (Double(num2) ?? 0)
